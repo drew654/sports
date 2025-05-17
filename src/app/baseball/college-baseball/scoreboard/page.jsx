94891d61c5116fc9a59a7c529cf0dc78837e9c4f
drew654 <drewpusey654@gmail.com>
2025-05-17 10:51:57 -0500

    College baseball tiles

---
+++ src/app/baseball/college-baseball/scoreboard/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useEffect } from "react";
+import { fetchData } from "../../../../utilities";
+import CollegeBaseballCompetitionTile from "../../../../components/CollegeBaseballCompetitionTile";
+
+const CollegeBaseballScoreboard = () => {
+  const apiURL =
+    "https://site.api.espn.com/apis/site/v2/sports/baseball/college-baseball/scoreboard";
+
+  const [events, setEvents] = React.useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const data = await fetchData(apiURL);
+      setEvents(data["events"] || []);
+    };
+    fetchEvents();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">College Baseball</h1>
+      <div className="grid grid-cols-1 gap-4">
+        {events.map((event, index) => (
+          <CollegeBaseballCompetitionTile
+            key={index}
+            competition={event.competitions[0]}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollegeBaseballScoreboard;
