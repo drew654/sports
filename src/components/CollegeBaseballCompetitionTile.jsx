6916c7cd40bd1538f82b687d3e64600a11fa140d
drew654 <drewpusey654@gmail.com>
2025-05-18 00:05:49 -0500

    Final status

--- src/components/CollegeBaseballCompetitionTile.jsx
+++ src/components/CollegeBaseballCompetitionTile.jsx
@@ -1,17 +1,32 @@
 const CollegeBaseballCompetitionTile = ({ competition }) => {
   return (
     <div className="p-4 border rounded shadow">
-      <div className="flex justify-between items-center w-50">
-        <h2 className="text-lg font-bold truncate max-w-[150px]" title={competition.competitors[0].team.displayName}>
-          {competition.competitors[0].team.displayName}
-        </h2>
-        <h2 className="font-mono">{competition.competitors[0].score}</h2>
-      </div>
-      <div className="flex justify-between items-center w-50">
-        <h2 className="text-lg font-bold truncate max-w-[150px]" title={competition.competitors[1].team.displayName}>
-          {competition.competitors[1].team.displayName}
-        </h2>
-        <h2 className="font-mono">{competition.competitors[1].score}</h2>
+      <div className="flex items-center">
+        <div className="pr-4">
+          <div className="flex justify-between items-center w-50">
+            <h2
+              className="text-lg font-bold truncate max-w-[150px]"
+              title={competition.competitors[0].team.displayName}
+            >
+              {competition.competitors[0].team.displayName}
+            </h2>
+            <h2 className="font-mono">{competition.competitors[0].score}</h2>
+          </div>
+          <div className="flex justify-between items-center w-50">
+            <h2
+              className="text-lg font-bold truncate max-w-[150px]"
+              title={competition.competitors[1].team.displayName}
+            >
+              {competition.competitors[1].team.displayName}
+            </h2>
+            <h2 className="font-mono">{competition.competitors[1].score}</h2>
+          </div>
+        </div>
+        <div className="border-l p-4">
+          {competition.status.type.name === "STATUS_FINAL" && (
+            <h2>{competition.status.type.description}</h2>
+          )}
+        </div>
       </div>
     </div>
   );
