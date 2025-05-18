b50273b6ce9a23538c7ed08ddf886ed9074b2d4f
drew654 <drewpusey654@gmail.com>
2025-05-18 08:47:00 -0500

    Refactor and fix truncation

--- src/components/CollegeBaseballCompetitionTile.jsx
+++ src/components/CollegeBaseballCompetitionTile.jsx
@@ -1,35 +1,32 @@
 const CollegeBaseballCompetitionTile = ({ competition }) => {
   return (
     <div className="p-4 border rounded shadow">
-      <div className="flex items-center">
-        <div className="pr-4 w-full">
-          <div className="flex justify-between items-center">
-            <h2
-              className="text-lg font-bold truncate max-w-[150px]"
-              title={competition.competitors[0].team.displayName}
-            >
-              {competition.competitors[0].team.displayName}
-            </h2>
-            <h2 className="font-mono font-bold">
-              {competition.competitors[0].score}
-            </h2>
-          </div>
-          <div className="flex justify-between items-center">
-            <h2
-              className="text-lg font-bold truncate max-w-[150px]"
-              title={competition.competitors[1].team.displayName}
-            >
-              {competition.competitors[1].team.displayName}
-            </h2>
-            <h2 className="font-mono font-bold">
-              {competition.competitors[1].score}
-            </h2>
-          </div>
+      <div className="flex items-center justify-between">
+        <div className="flex-1 min-w-0">
+          <h2 className="text-lg font-bold truncate">
+            {competition.competitors[0].team.displayName}
+          </h2>
+          <h2 className="text-lg font-bold truncate">
+            {competition.competitors[1].team.displayName}
+          </h2>
         </div>
-        <div className="border-l p-4">
-          {competition.status.type.name === "STATUS_FINAL" && (
-            <h2>{competition.status.type.description}</h2>
-          )}
+        <div className="pl-4">
+          <div className="flex items-center">
+            <div>
+              <h2 className="font-mono font-bold">
+                {competition.competitors[0].score}
+              </h2>
+              <h2 className="font-mono font-bold">
+                {competition.competitors[1].score}
+              </h2>
+            </div>
+            <div className="mx-2" />
+            {competition.status.type.name === "STATUS_FINAL" && (
+              <h2 className="border-l p-4">
+                {competition.status.type.description}
+              </h2>
+            )}
+          </div>
         </div>
       </div>
     </div>
