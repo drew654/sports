6e0ce1a71ff093d4671c4264bc197d7649dba4a9
drew654 <drewpusey654@gmail.com>
2025-05-17 23:52:47 -0500

    Lined up scores

--- src/app/baseball/college-baseball/scoreboard/page.jsx
+++ src/app/baseball/college-baseball/scoreboard/page.jsx
@@ -18,7 +18,7 @@ const CollegeBaseballScoreboard = () => {
   }, []);
 
   return (
-    <div>
+    <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">College Baseball</h1>
       <div className="grid grid-cols-1 gap-4">
         {events.map((event, index) => (
--- src/components/CollegeBaseballCompetitionTile.jsx
+++ src/components/CollegeBaseballCompetitionTile.jsx
@@ -1,30 +1,18 @@
 const CollegeBaseballCompetitionTile = ({ competition }) => {
   return (
     <div className="p-4 border rounded shadow">
-      <table className="w-full">
-        <tbody>
-          <tr>
-            <td className="text-left pr-4">
-              <h2 className="text-lg font-bold">
-                {competition.competitors[0].team.displayName}
-              </h2>
-            </td>
-            <td className="text-right w-12 align-middle">
-              <p className="font-mono">{competition.competitors[0].score}</p>
-            </td>
-          </tr>
-          <tr>
-            <td className="text-left pr-4">
-              <h2 className="text-lg font-bold">
-                {competition.competitors[1].team.displayName}
-              </h2>
-            </td>
-            <td className="text-right w-12 align-middle">
-              <p className="font-mono">{competition.competitors[1].score}</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="flex justify-between items-center w-50">
+        <h2 className="text-lg font-bold truncate max-w-[150px]" title={competition.competitors[0].team.displayName}>
+          {competition.competitors[0].team.displayName}
+        </h2>
+        <h2 className="font-mono">{competition.competitors[0].score}</h2>
+      </div>
+      <div className="flex justify-between items-center w-50">
+        <h2 className="text-lg font-bold truncate max-w-[150px]" title={competition.competitors[1].team.displayName}>
+          {competition.competitors[1].team.displayName}
+        </h2>
+        <h2 className="font-mono">{competition.competitors[1].score}</h2>
+      </div>
     </div>
   );
 };
