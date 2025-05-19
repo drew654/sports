d008930c4ab4f5f031ff6508c5f06dc353303d5e
drew654 <drewpusey654@gmail.com>
2025-05-18 20:38:42 -0500

    Logos

--- src/components/CollegeBaseballCompetitionTile.jsx
+++ src/components/CollegeBaseballCompetitionTile.jsx
@@ -5,9 +5,16 @@ const CollegeBaseballCompetitionTile = ({ competition }) => {
         <div className="flex-1 min-w-0 pl-2">
           {[0, 1].map((i) => (
             <div key={i} className="flex items-center justify-between ">
-              <h2 className="text-base font-bold truncate min-w-0 select-none">
-                {competition.competitors[i].team.displayName}
-              </h2>
+              <div className="flex items-center">
+                <img
+                  src={competition.competitors[i].team.logo}
+                  alt={competition.competitors[i].team.displayName}
+                  className="w-4 h-4 mr-2"
+                />
+                <h2 className="text-base font-bold truncate min-w-0 select-none">
+                  {competition.competitors[i].team.displayName}
+                </h2>
+              </div>
               <h2 className="font-mono font-bold select-none px-4 text-base">
                 {competition.competitors[i].score}
               </h2>
