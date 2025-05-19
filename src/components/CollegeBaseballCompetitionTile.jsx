83d16483f68cb2ba1c5a687cbdf81d76a3f95a76
drew654 <drewpusey654@gmail.com>
2025-05-18 20:29:08 -0500

    Notes

--- src/components/CollegeBaseballCompetitionTile.jsx
+++ src/components/CollegeBaseballCompetitionTile.jsx
@@ -1,23 +1,28 @@
 const CollegeBaseballCompetitionTile = ({ competition }) => {
   return (
-    <div className="flex items-center p-4 border rounded shadow select-none min-w-0">
-      <div className="flex-1 min-w-0">
-        {[0, 1].map((i) => (
-          <div key={i} className="flex items-center justify-between ">
-            <h2 className="text-lg font-bold truncate min-w-0 select-none">
-              {competition.competitors[i].team.displayName}
-            </h2>
-            <h2 className="font-mono font-bold select-none px-4">
-              {competition.competitors[i].score}
-            </h2>
-          </div>
-        ))}
+    <div className="border rounded shadow p-2">
+      <div className="flex items-center select-none min-w-0">
+        <div className="flex-1 min-w-0 pl-2">
+          {[0, 1].map((i) => (
+            <div key={i} className="flex items-center justify-between ">
+              <h2 className="text-base font-bold truncate min-w-0 select-none">
+                {competition.competitors[i].team.displayName}
+              </h2>
+              <h2 className="font-mono font-bold select-none px-4 text-base">
+                {competition.competitors[i].score}
+              </h2>
+            </div>
+          ))}
+        </div>
+        {competition.status.type.name === "STATUS_FINAL" && (
+          <h2 className="border-l p-2 text-center select-none text-base">
+            {competition.status.type.description}
+          </h2>
+        )}
       </div>
-      {competition.status.type.name === "STATUS_FINAL" && (
-        <h2 className="border-l p-4 text-center select-none">
-          {competition.status.type.description}
-        </h2>
-      )}
+      <h3 className="pt-2 pl-2 text-xs select-none">
+        {competition.notes[0].headline}
+      </h3>
     </div>
   );
 };
