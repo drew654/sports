6812be95ac26dba4fecb57aec198db68a84012c6
drew654 <drewpusey654@gmail.com>
2025-05-18 21:24:33 -0500

    Start date selector

--- src/app/baseball/college-baseball/scoreboard/page.jsx
+++ src/app/baseball/college-baseball/scoreboard/page.jsx
@@ -1,6 +1,10 @@
 "use client";
 import React, { useEffect } from "react";
-import { fetchData } from "../../../../utilities";
+import {
+  fetchData,
+  formatDateToMonthDay,
+  getDayAbbreviation,
+} from "../../../../utilities";
 import CollegeBaseballCompetitionTile from "../../../../components/CollegeBaseballCompetitionTile";
 
 const CollegeBaseballScoreboard = () => {
@@ -8,18 +12,36 @@ const CollegeBaseballScoreboard = () => {
     "https://site.api.espn.com/apis/site/v2/sports/baseball/college-baseball/scoreboard";
 
   const [events, setEvents] = React.useState([]);
+  const [league, setLeague] = React.useState(null);
 
   useEffect(() => {
     const fetchEvents = async () => {
       const data = await fetchData(apiURL);
       setEvents(data["events"] || []);
+      setLeague(
+        data["leagues"].find((league) => league.name === "NCAA Baseball")
+      );
     };
     fetchEvents();
   }, []);
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4 select-none">College Baseball</h1>
+      <h1 className="text-2xl font-bold mb-4 select-none">NCAA Baseball</h1>
+      {league && (
+        <div className="flex mb-4 overflow-x-auto max-w-full">
+          {league.calendar.map((day) => (
+            <div key={day} className="border flex-shrink-0 p-2 text-center">
+              <h2 className="text-center select-none">
+                {getDayAbbreviation(day)}
+              </h2>
+              <h2 className="text-center select-none">
+                {formatDateToMonthDay(day)}
+              </h2>
+            </div>
+          ))}
+        </div>
+      )}
       <div className="grid grid-cols-1 gap-4">
         {events.map((event, index) => (
           <CollegeBaseballCompetitionTile
--- src/utilities.js
+++ src/utilities.js
@@ -10,3 +10,20 @@ export async function fetchData(url) {
     return {};
   }
 }
+
+export function getDayAbbreviation(date) {
+  const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const d = date instanceof Date ? date : new Date(date);
+  return days[d.getDay()];
+}
+
+export function formatDateToMonthDay(date) {
+  const d = date instanceof Date ? date : new Date(date);
+  const months = [
+    "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+    "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
+  ];
+  const month = months[d.getMonth()];
+  const day = d.getDate();
+  return `${month} ${day}`;
+}
