ebb4f71f551a5e1078285bda306b9f5cae8050a8
drew654 <drewpusey654@gmail.com>
2025-05-25 16:25:28 -0500

    Home screen

---
+++ src/app/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+const Sports = () => {
+  const sports = [
+    { name: "NCAA Baseball", path: "/baseball/college-baseball/scoreboard" },
+  ];
+
+  return (
+    <div className="p-4">
+      {sports.map((sport) => (
+        <div
+          key={sport.name}
+          className="p-4 border rounded-lg mb-4 cursor-pointer"
+          onClick={() => (window.location.href = sport.path)}
+        >
+          <h1 className="text-2xl font-bold select-none">{sport.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Sports;
