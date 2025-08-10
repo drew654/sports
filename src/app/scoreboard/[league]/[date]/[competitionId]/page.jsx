f133ea138b24cc0d6bb0dd0cee588ba1b342e1e1
drew654 <drewpusey654@gmail.com>
2025-08-10 18:21:14 -0500

    Fix undefined outs text bug

--- src/app/scoreboard/[league]/[date]/[competitionId]/page.jsx
+++ src/app/scoreboard/[league]/[date]/[competitionId]/page.jsx
@@ -63,9 +63,12 @@ const BaseballCompetitionPage = ({ params }) => {
                 <h2 className="text-xs font-bold">
                   {competition.status.type.shortDetail}
                 </h2>
-                {competition.situation.lastPlay.type.type !== "end-inning" && (
-                  <h2 className="text-xs font-bold">{competition.outsText}</h2>
-                )}
+                {competition.situation &&
+                  competition.situation.lastPlay.type.type !== "end-inning" && (
+                    <h2 className="text-xs font-bold">
+                      {competition.outsText}
+                    </h2>
+                  )}
               </>
             )}
           </div>
@@ -89,60 +92,60 @@ const BaseballCompetitionPage = ({ params }) => {
           <BaseballScoringSummary competition={competition} />
         )}
         {competition.status.type.name === "STATUS_IN_PROGRESS" &&
-        competition.situation.dueUp ? (
-          <div className="mt-4 flex">
-            <div className="flex-1">
-              <h3 className="font-bold">
-                DUE UP ({competition.situation.dueUp[0].batOrder})
-              </h3>
-              <h4 className="text-sm">
-                {competition.situation.dueUp[0].athlete.shortName}
-              </h4>
-            </div>
-            <div className="flex-1">
-              <h3 className="font-bold">
-                DUE UP ({competition.situation.dueUp[1].batOrder})
-              </h3>
-              <h4 className="text-sm">
-                {competition.situation.dueUp[1].athlete.shortName}
-              </h4>
-            </div>
-            <div className="flex-1">
-              <h3 className="font-bold">
-                DUE UP ({competition.situation.dueUp[2].batOrder})
-              </h3>
-              <h4 className="text-sm">
-                {competition.situation.dueUp[2].athlete.shortName}
-              </h4>
-            </div>
-          </div>
-        ) : (
-          <div className="mt-4 flex">
-            <div className="flex-1">
-              <h3 className="font-bold">PITCHER</h3>
-              <h4 className="text-sm">
-                {competition.situation.pitcher.athlete.shortName}
-              </h4>
-            </div>
-            <div className="flex-1">
-              <h3 className="font-bold">BATTER</h3>
-              <h4 className="text-sm">
-                {competition.situation.batter.athlete.shortName}
-              </h4>
-            </div>
-            <div className="flex-1">
-              <div className="flex items-center">
-                <h4 className="pr-2">B</h4>
-                <BaseballBalls count={competition.situation.balls} />
+          (competition.situation.dueUp ? (
+            <div className="mt-4 flex">
+              <div className="flex-1">
+                <h3 className="font-bold">
+                  DUE UP ({competition.situation.dueUp[0].batOrder})
+                </h3>
+                <h4 className="text-sm">
+                  {competition.situation.dueUp[0].athlete.shortName}
+                </h4>
               </div>
-              <div className="flex items-center">
-                <h4 className="pr-2">S</h4>
-                <BaseballStrikes count={competition.situation.strikes} />
+              <div className="flex-1">
+                <h3 className="font-bold">
+                  DUE UP ({competition.situation.dueUp[1].batOrder})
+                </h3>
+                <h4 className="text-sm">
+                  {competition.situation.dueUp[1].athlete.shortName}
+                </h4>
+              </div>
+              <div className="flex-1">
+                <h3 className="font-bold">
+                  DUE UP ({competition.situation.dueUp[2].batOrder})
+                </h3>
+                <h4 className="text-sm">
+                  {competition.situation.dueUp[2].athlete.shortName}
+                </h4>
               </div>
             </div>
-            <BaseballBases situation={competition.situation} />
-          </div>
-        )}
+          ) : (
+            <div className="mt-4 flex">
+              <div className="flex-1">
+                <h3 className="font-bold">PITCHER</h3>
+                <h4 className="text-sm">
+                  {competition.situation.pitcher.athlete.shortName}
+                </h4>
+              </div>
+              <div className="flex-1">
+                <h3 className="font-bold">BATTER</h3>
+                <h4 className="text-sm">
+                  {competition.situation.batter.athlete.shortName}
+                </h4>
+              </div>
+              <div className="flex-1">
+                <div className="flex items-center">
+                  <h4 className="pr-2">B</h4>
+                  <BaseballBalls count={competition.situation.balls} />
+                </div>
+                <div className="flex items-center">
+                  <h4 className="pr-2">S</h4>
+                  <BaseballStrikes count={competition.situation.strikes} />
+                </div>
+              </div>
+              <BaseballBases situation={competition.situation} />
+            </div>
+          ))}
       </div>
     )
   );
