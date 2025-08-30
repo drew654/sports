f7354b74c63a941bb4b2c508fbb5fa30111660e8
Drew Pusey <drewpusey654@gmail.com>
2025-08-30 00:05:17 -0500

    Put scoring summary into component

--- src/app/scoreboard/[sport]/[league]/[date]/[competitionId]/page.jsx
+++ src/app/scoreboard/[sport]/[league]/[date]/[competitionId]/page.jsx
@@ -7,11 +7,11 @@ import BaseballBases from "../../../../../../components/BaseballBases";
 import BaseballBalls from "../../../../../../components/BaseballBalls";
 import BaseballStrikes from "../../../../../../components/BaseballStrikes";
 import NumberLineGraph from "../../../../../../components/NumberLineGraph";
+import BaseballScoringSummary from "../../../../../../components/BaseballScoringSummary";
 import {
   decimalToWinningTeamPercentage,
   lastArrayElement,
 } from "../../../../../../utilities";
-import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
 
 const BaseballCompetitionPage = ({ params }) => {
   const unwrappedParams = React.use(params);
@@ -220,54 +220,7 @@ const BaseballCompetitionPage = ({ params }) => {
           </div>
         )}
         {plays?.length > 0 && (
-          <div className="pt-8">
-            <h1 className="text-xl font-bold">Scoring Summary</h1>
-            <table className="w-full">
-              <thead>
-                <tr>
-                  {[...Array(4)].map((_, index) => (
-                    <th key={index}></th>
-                  ))}
-                  <th>{competition.competitors[0].team.abbreviation}</th>
-                  <th>{competition.competitors[1].team.abbreviation}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {plays.map((play, index) => (
-                  <tr key={index} className="border-t border-b">
-                    <td className="p-2 h-10 w-10">
-                      <img
-                        src={
-                          competition.competitors[
-                            play.period.type === "Top" ? 0 : 1
-                          ].team.logo
-                        }
-                        alt={
-                          competition.competitors[
-                            play.period.type === "Top" ? 0 : 1
-                          ].team.displayName
-                        }
-                        className="w-5 h-5"
-                      />
-                    </td>
-                    <td className="p-2">
-                      {play.period.type === "Top" ? (
-                        <ChevronUpIcon className="h-4 w-4" />
-                      ) : (
-                        <ChevronDownIcon className="h-4 w-4" />
-                      )}
-                    </td>
-                    <td className="p-2">
-                      {play.period.displayValue.split(" ")[0]}
-                    </td>
-                    <td className="p-2">{play.text}</td>
-                    <td className="p-2">{play.awayScore}</td>
-                    <td className="p-2">{play.homeScore}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <BaseballScoringSummary competition={competition} plays={plays} />
         )}
       </div>
     )
